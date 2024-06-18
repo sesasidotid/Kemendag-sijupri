90ba1b695b47ecd786b1e1e314d2966d4cdb320d
goldianGPP <goldiangabepakpahan@gmail.com>
2024-06-18 12:58:03 +0700

    add seed

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Crypt;
@@ -12,17 +13,15 @@ class TestSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('tbl_user')->insert([
-            [
-                'nip' => '111111111111111111',
-                'created_at' => now(),
-                'created_by' => "system",
-                'password' => Crypt::encrypt(Hash::make('111111111111111111')),
-                'name' => "SUPER ADMIN",
-                'role_code' => 'super_admin',
-                'app_code' => 'PUSBIN',
-                'access_method' => (object) ["read" => true, "create" => true, "delete" => true, "update" => true]
-            ]
+        User::create([
+            'nip' => '111111111111111111',
+            'created_at' => now(),
+            'created_by' => "system",
+            'password' => Crypt::encrypt(Hash::make('111111111111111111')),
+            'name' => "SUPER ADMIN",
+            'role_code' => 'super_admin',
+            'app_code' => 'PUSBIN',
+            'access_method' => (object) ["read" => true, "create" => true, "delete" => true, "update" => true]
         ]);
     }
 }
