5d09d69521e88c90111125937a6c8d268a968f45
gpakpahan <g.pakpahan@teravintech.com>
2024-11-11 20:49:48 +0700

    log

--- app/Http/Controllers/Formasi/FormasiController.php
+++ app/Http/Controllers/Formasi/FormasiController.php
@@ -68,7 +68,7 @@ class FormasiController extends Controller
                     'formasiListInactive'
                 ));
             case RoleCode::USER:
-                $formasiData = $this->formasi->findByJabatanCodeAndUnitKerjaIdAndTaskStatus($userContext->jabatan->id, $userContext->unitKerja->id, TaskStatus::APPROVE);
+                $formasiData = $this->formasi->findByJabatanCodeAndUnitKerjaIdAndTaskStatus($userContext->jabatan->code, $userContext->unitKerja->id, TaskStatus::APPROVE);
                 return view('formasi.jabatan.detail_2', compact('formasiData'));
         }
     }
